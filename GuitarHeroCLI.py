24326b42b226a5fc19eeac40f084f97f7dd66d91
Dylan Alexander <dylanjalexander2@gmail.com>
2023-11-25 12:57:28 -0500

    Deciding where to extract actual agent response

--- GuitarHeroCLI.py
+++ GuitarHeroCLI.py
@@ -1,8 +1,12 @@
 from openai import AzureOpenAI
 import os
 import requests
+from pathlib import Path
+import pandas as pd
 
 from guitar_hero_utils import normalize_text, get_embedding, cosine_similarity
+from transcript_utils import (get_transcript_data, get_transcript_turns, create_transcript_subsets,
+                              convert_subsets_to_messages)
 
 # Setup parameters to authenticate with OpenAI API
 api_key = os.getenv("AZURE_OPENAI_API_KEY")
@@ -18,37 +22,30 @@ url = f"{base_url}/openai/deployments?api-version=2022-12-01"
 r = requests.get(url, headers={"api-key": api_key})
 print("Confirming that an entry for Embedding & Language models exist in API response:\n", r.text)
 
-# Prompting ChatGPT to come up with a response to someone calling about an overflowing water heater
-messages = [
-    {"role": "system", "content": "Thank you for calling Nexidia heat and plumbing, how may i help you?"},
-    {"role": "user", "content": "Hi, my water-heater is overflowing."},
-    {"role": "system", "content": "Have you turned off water to the house?"},
-    {"role": "user", "content": "No, how do i do that?"},
-]
-# Generate the next best sentence
-next_best_sentence = client.chat.completions.create(model="Guitar-H3r0-GPT-Turbo", messages=messages)
+# Parse transcript
+transcript_path = Path('/Users/dylanalexander/Repos/GuitarH3r0/Call-Center-Transcript.CSV')
+transcript_df: pd.DataFrame = get_transcript_data(str(transcript_path))
+turns, speakers = get_transcript_turns(transcript_df)
+transcript_subsets = create_transcript_subsets(turns, speakers)
+all_transcript_messages = convert_subsets_to_messages(transcript_subsets)
 
-# Two responses to compare to the one suggested by ChatGPT:
-response_1 = "Do you know where your water meter is located?"
-response_2 = "In Louisiana, I like to eat Po-boy sandwiches"
+# Generate the next best sentence with ChatGPT
+for message, actual_agent_response in zip(all_transcript_messages, ):
+    next_best_sentence = client.chat.completions.create(model="Guitar-H3r0-GPT-Turbo", messages=message)
 
-print("\nConversation: \n", [x["content"] for x in messages])
-print("\nSample Response 1: ", f'{response_1}')
-print("Sample Response 2: ", f'{response_2}\n')
-print("'Next-Best-Sentence' According to ChatGPT:\n", next_best_sentence.choices[0].message.content)
-print("*Normalized* 'Next-Best-Sentence' According to ChatGPT:\n",
-      normalize_text(next_best_sentence.choices[0].message.content))
+    print("\nConversation: \n", [x["content"] for x in message])
+    print("\nActual Agent Response: ", f'{actual_agent_response}')
+    print("'Next-Best-Sentence' According to ChatGPT:\n", next_best_sentence.choices[0].message.content)
+    print("*Normalized* 'Next-Best-Sentence' According to ChatGPT:\n",
+          normalize_text(next_best_sentence.choices[0].message.content))
 
-# Generate Embeddings for sentences
-gpt_response_embedding = get_embedding(next_best_sentence.choices[0].message.content, client)
-response_one_embedding = get_embedding(response_1, client)
-response_two_embedding = get_embedding(response_2, client)
+    # Generate Embeddings for sentences
+    gpt_response_embedding = get_embedding(next_best_sentence.choices[0].message.content, client)
+    actual_agent_response_embedding = get_embedding(actual_agent_response, client)
 
-# Calculate distance between embeddings
-dist_0_0 = cosine_similarity(gpt_response_embedding, gpt_response_embedding)
-dist_0_1 = cosine_similarity(gpt_response_embedding, response_one_embedding)
-dist_0_2 = cosine_similarity(gpt_response_embedding, response_two_embedding)
+    # Calculate distance between embeddings
+    dist_0_0 = cosine_similarity(gpt_response_embedding, gpt_response_embedding)
+    dist_0_1 = cosine_similarity(gpt_response_embedding, actual_agent_response_embedding)
 
-print(f'\nDistance (gpt_response_embedding, gpt_response_embedding) = {dist_0_0:0.3}')
-print(f'Distance (gpt_response_embedding, response_one_embedding) = {dist_0_1:0.3}')
-print(f'Distance (gpt_response_embedding, response_two_embedding) = {dist_0_2:0.3}')
+    print(f'\nDistance (gpt_response_embedding, gpt_response_embedding) = {dist_0_0:0.3}')
+    print(f'Distance (gpt_response_embedding, actual_agent_response_embedding) = {dist_0_1:0.3}')
--- transcript_utils.py
+++ transcript_utils.py
@@ -36,6 +36,8 @@ def get_transcript_turns(transcript_df: pd.DataFrame) -> Tuple[List[str], List[i
                 current_speaker = speaker
                 current_phrase_parts = []  # Reset current_phrase_parts
 
+            current_phrase_parts.append(phrase)
+
         # Account for the final phrase
         if current_phrase_parts:
             phrases_per_turn.append(' '.join(current_phrase_parts) + " ")
@@ -45,3 +47,46 @@ def get_transcript_turns(transcript_df: pd.DataFrame) -> Tuple[List[str], List[i
     except Exception as e:
         raise TranscriptParsingError("An error occurred while parsing the transcript",
                                      original_exception=e) from e
+
+
+def create_transcript_subsets(phrases_per_turn: List[str], speaker_per_turn: List[int]) -> List[List[Tuple[str, int]]]:
+    """
+
+    :param phrases_per_turn:
+    :param speaker_per_turn:
+    :return:
+    """
+    # [Set of 5-6 turns, each subset of turns ends with the customer as the speaker.]
+    # 5-6 turns should provide enough context in the conversation for ChatGPT to provide a reasonable response
+    transcript_subsets = []
+    subset = []
+    counter = 0
+    for turn, speaker in zip(phrases_per_turn, speaker_per_turn):
+        if counter <= 4:
+            subset.append((turn, speaker))
+            # If the last turn for this subset ends with the agent speaking, extend the count by 1
+            if counter == 4 and speaker == 0:
+                # This ensures that all turn subsets will end with a customer response/request
+                counter -= 1
+            counter += 1  # Increment counter
+        else:
+            transcript_subsets.append(subset)
+            subset = [(turn, speaker)]  # Reset the subset array and fill it with this iterations turn
+            counter = 0  # Reset counter
+
+    return transcript_subsets
+
+
+def convert_subsets_to_messages(transcript_subsets: List[List[Tuple[str, int]]]):
+    all_transcript_messages = []
+    for subset in transcript_subsets:
+        subset_messages = []
+        for turn, speaker in subset:
+            if speaker == 0:
+                subset_messages.append({"role": "system", "content": turn})
+            elif speaker == 1:
+                subset_messages.append({"role": "user", "content": turn})
+
+        all_transcript_messages.append(subset_messages)
+
+    return all_transcript_messages
